68a8c61a2c76b808b10a8cff501f4716b968a3d1
Gleb Shevchenko <glebosh@Glebs-MacBook-Air.local>
2026-05-20 10:22:06 +0300

    feat: replace runserver with gunicorn + collectstatic

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
-set -e
+#!/bin/sh                                                                                                                                                                                                                                      
+  set -e                                                                                                                                                                                                                                       
 
-echo "Running migrations..."
-python manage.py migrate
+  echo "Running migrations..."
+  python manage.py migrate --noinput
 
-echo "Starting server..."
-python manage.py runserver 0.0.0.0:8000
+  echo "Collecting static files..."                                                                                                                                                                                                              
+  python manage.py collectstatic --noinput
+                                                                                                                                                                                                                                                 
+  echo "Starting Gunicorn..."                                                                                                                                                                                                                  
+  exec gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --workers 3 --access-logfile - --error-logfile -
